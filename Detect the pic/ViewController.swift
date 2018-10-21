591284baf8e402df7d1a5f68d120cd5eeecc1ad1
Andrés De La Ossa <ossa@andress-macbook-pro.local>
2018-10-21 13:28:55 -0500

    Initial Commit

---
+++ Detect the pic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Detect the pic
+//
+//  Created by Andres Felipe De La Ossa Navarro on 10/21/18.
+//  Copyright © 2018 Andres Felipe De La Ossa Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
